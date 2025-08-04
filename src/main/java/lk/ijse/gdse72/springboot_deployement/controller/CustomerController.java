8d5253bba5a058b198fc99301484f4086c0b2fd2
Rethmi Thennakoon <reththennakoon.edu@gmail.com>
2025-08-04 11:41:52 +0530

    create crud

--- src/main/java/lk/ijse/gdse72/springboot_deployement/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse72/springboot_deployement/controller/CustomerController.java
@@ -7,10 +7,7 @@ import lk.ijse.gdse72.springboot_deployement.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customer")
@@ -19,7 +16,7 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<APIResponse<String>> getCustomer(@RequestBody CustomerDTO customerDTO) {
         customerService.saveCustomer(customerDTO);
 
