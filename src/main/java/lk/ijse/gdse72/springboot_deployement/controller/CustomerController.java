9b92364ca2c577653f876fa5b9400064df88f397
Rethmi Thennakoon <reththennakoon.edu@gmail.com>
2025-08-04 10:36:35 +0530

    updated

--- src/main/java/lk/ijse/gdse72/springboot_deployement/controller/CustomerController.java
+++ src/main/java/lk/ijse/gdse72/springboot_deployement/controller/CustomerController.java
@@ -1,4 +1,16 @@
 package lk.ijse.gdse72.springboot_deployement.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    @GetMapping
+    public String getCustomer() {
+        return "Customer 1";
+    }
 }
